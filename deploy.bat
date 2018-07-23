0aa6ca4629f9264c85dc59b1a94758e5a383c022
Leo Chiu <block101academy@gmail.com>
2018-07-23 23:25:53 +0800

    group assets for github pages

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 robocopy ./src/ ./docs /s /e
-robocopy ./build/contracts/* ./docs /s /e
+robocopy ./build/contracts/ ./docs /s /e
 git add .
 git commit -m "group assets for github pages"
 git push -u origin master
